709f39ee35e3bc8be486dec25d701f925b5c98eb
zoubingwu <zoubingwu@gmail.com>
2025-04-12 18:51:42 +0800

    initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import { Greet } from "../wailsjs/go/main/App";
+
+function App() {
+  const [resultText, setResultText] = useState(
+    "Please enter your name below 👇",
+  );
+  const [name, setName] = useState("");
+  const updateName = (e: any) => setName(e.target.value);
+  const updateResultText = (result: string) => setResultText(result);
+
+  const greet = () => {
+    Greet(name).then(updateResultText);
+  };
+
+  return (
+    <div id="App">
+      <div id="result" className="result">
+        {resultText}
+      </div>
+      <div id="input" className="input-box">
+        <input
+          id="name"
+          className="input"
+          onChange={updateName}
+          autoComplete="off"
+          name="input"
+          type="text"
+        />
+        <button className="btn" onClick={greet}>
+          Greet
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
