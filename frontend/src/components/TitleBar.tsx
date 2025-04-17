1cdf047798b2ca9bdd763fed8fba1cdb77bf7720
zoubingwu <zoubingwu@gmail.com>
2025-04-18 02:47:23 +0800

    Add double-click functionality to TitleBar for window maximize/unmaximize
    - Implemented onDoubleClick event to toggle window state between maximized and unmaximized using Wails runtime methods.
    - Enhances user interaction by allowing window resizing through double-clicking the TitleBar.

--- frontend/src/components/TitleBar.tsx
+++ frontend/src/components/TitleBar.tsx
@@ -1,8 +1,21 @@
 import React, { memo } from "react";
+import {
+  WindowMaximise,
+  WindowUnmaximise,
+  WindowIsMaximised,
+} from "wailsjs/runtime";
 
 function TitleBar({ title }: { title: string }) {
+  const onDoubleClick = async () => {
+    if (await WindowIsMaximised()) {
+      WindowUnmaximise();
+    } else {
+      WindowMaximise();
+    }
+  };
   return (
     <div
+      onDoubleClick={onDoubleClick}
       className="h-[28px] bg-[var(--card)] text-[var(--card-foreground)] flex justify-center items-center select-none text-xs border-b border-[var(--border)] flex-shrink-0"
       style={{ "--wails-draggable": "drag" } as React.CSSProperties}
     >
