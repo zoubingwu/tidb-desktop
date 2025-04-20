9e3b3c073c3c954b2c13bd52e4dcd2d6c55095ed
zoubingwu <zoubingwu@gmail.com>
2025-04-21 01:08:03 +0800

    Add vite-tsconfig-paths dependency and update Vite config
    - Added `vite-tsconfig-paths` version 5.1.4 to `package.json` for improved TypeScript path resolution in Vite projects.
    - Updated `vite.config.ts` to include `vite-tsconfig-paths` in the plugins array, enhancing module resolution and simplifying imports.
    - Updated `pnpm-lock.yaml` to reflect the new dependency and its version.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,17 +1,11 @@
-import path from "node:path";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-      wailsjs: path.resolve(__dirname, "./wailsjs"),
-    },
-  },
+  plugins: [react(), tailwindcss(), tsconfigPaths()],
   // https://github.com/wailsapp/wails/issues/3064#issuecomment-2053632869
   server: {
     hmr: {
