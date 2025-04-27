d14b3a4f757b256923de4df3a3b00a1c9f082e7e
zoubingwu <zoubingwu@gmail.com>
2025-04-28 06:41:11 +0800

    style: update DataTable table width
    - Changed the table class in the DataTable component from `h-(--table-height)` to `min-w-full` for improved responsiveness and layout consistency.
    - This adjustment ensures the table utilizes the full width of its container, enhancing the overall user interface.

--- frontend/src/components/DataTable.tsx
+++ frontend/src/components/DataTable.tsx
@@ -28,7 +28,7 @@ function DataTable<TData>({ table, height }: DataTableProps<TData>) {
   return (
     <div ref={parentRef} className="overflow-auto" style={{ height }}>
       <div style={{ height: `${virtualizer.getTotalSize()}px` }}>
-        <table className="text-sm after:inline-block after:h-(--table-height)">
+        <table className="min-w-full text-sm after:inline-block after:h-(--table-height)">
           <TableHeader className="sticky z-50 top-0 bg-background shadow-sm">
             {table.getHeaderGroups().map((headerGroup) => (
               <TableRow key={headerGroup.id}>
